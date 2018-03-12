b217877bcefd3504bc5e4e5f943f9b10b93ace01
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-03-12 14:03:46 -0700

    created a running total variable for the shopping cart.

--- vending-machine/src/App.js
+++ vending-machine/src/App.js
@@ -7,24 +7,29 @@ class App extends Component {
 
     this.state = {
       inventory: [['Twix', 2], ['Jerky', 3], ['Soda', 1.5], ['Chips', 1.25]],
-      shoppingCart: []
+      shoppingCart: [],
+      total: 0
     };
 
     this.checkout = this.checkout.bind(this);
     this.addProduct = this.addProduct.bind(this);
   }
 
-  addProduct(index, item) {
+  addProduct(index, item, cost) {
     let prevCart = this.state.shoppingCart;
+    let prevTotal = this.state.total;
 
     prevCart.push(item)
+    prevTotal += cost
 
     this.setState({
-      shoppingCart: prevCart
+      shoppingCart: prevCart,
+      total: prevTotal
     });
   }
 
   checkout() {
+    alert(`Thank you for purchasing ${ this.state.shoppingCart }`)
     console.log(this.state.shoppingCart)
   }
 
@@ -34,23 +39,25 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">React Vending Machine</h1>
         </header>
-        <ul>
+        <h1>Items to purchase</h1>
+        <ul className="inventoryList">
           { this.state.inventory.map((item, i) => (
             <li key={i}>
               <span>Item: {item[0]} </span>
               <span>Cost: {item[1]} </span>
-              <button onClick={() => this.addProduct(i, item[0])}>+</button>
+              <button onClick={() => this.addProduct(i, item[0], item[1])}>+</button>
             </li>
           )) }
         </ul>
         <h1>Shopping Cart</h1>
-        <ul>
+        <ul className="shoppingCart">
           { this.state.shoppingCart.map((item, i) => (
-            <li key={i}>
+            <li className="shoppingCartItem">
               <span>Item: {item} </span>
             </li>
           )) }
         </ul>
+        <p> Total: { this.state.total }</p> 
         <button onClick={() => this.checkout()}>Checkout</button>
       </div>
     );
